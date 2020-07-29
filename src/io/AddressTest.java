86747fb7b47170c3b799bc038c7f54892a760727
Administrator <Administrator@119.193.204.52>
2020-07-29 19:55:41 +0900

    ㅁ

--- src/io/AddressTest.java
+++ src/io/AddressTest.java
@@ -12,96 +12,72 @@ import java.util.Map;
 import com.erp.test.common.Connector;
 
 public class AddressTest {
-	// 법정코드, 시도명,
-	public static void main(String[] args) throws SQLException {
-		String key = "DONG_CODE\r\n" + "SIDO\r\n" + "GUGUN\r\n" + "DONG_NAME\r\n" + "LEE_NAME\r\n" + "IS_MNT\r\n"
-				+ "JIBUN\r\n" + "SUB_JIBUN\r\n" + "ROAD_CODE\r\n" + "ROAD_NAME\r\n" + "IS_BASE\r\n" + "BULID_NUM\r\n"
-				+ "SUN_BULID_NUM\r\n" + "BULIDING_NAME\r\n" + "DETAIL_BULIDING_NAME\r\n" + "ADDR_CODE";
-		String[] keys = key.split("\r\n");
-<<<<<<< HEAD
-		String str = ReadText.readText("C:\\studyfile\\address\\build_sejong.txt");
-		String[] strs = str.split("<>");
-		String[] values = null;
-		List<Map<String, String>> list = new ArrayList<>();
-		for (int i = 0; i < strs.length; i++) {
-			Map<String, String> map = new LinkedHashMap<>();
-			values = strs[i].split("\\|");
-			for (int j = 0; j < keys.length; j++) {
-				map.put(keys[j], values[j]);
-			}
-			list.add(map);
-		}
-		System.out.println(list);
-		long sTime = System.currentTimeMillis();
-		String sql = " insert into address(";
-		String value = " values(";
-		for (String Key : keys) {
-			sql += Key + ",";
-			value += "?,";
-		}
-		sql = sql.substring(0, sql.length() - 1) + ")\r\n";
-		value = value.substring(0, value.length() - 1) + ")";
-		sql += value;
-		Connection conn = Connector.open();
-		PreparedStatement ps = conn.prepareStatement(sql);
-		for (Map<String, String> row : list) {
-			for (int i = 0; i < keys.length; i++) {
-				ps.setString((i + 1), row.get(keys[i]));
-			}
-			ps.executeUpdate();
-=======
+   // 법정코드, 시도명,
+   public static void main(String[] args) throws SQLException {
+      String key = "DONG_CODE\r\n" + "SIDO\r\n" + "GUGUN\r\n" + "DONG_NAME\r\n" + "LEE_NAME\r\n" + "IS_MNT\r\n"
+            + "JIBUN\r\n" + "SUB_JIBUN\r\n" + "ROAD_CODE\r\n" + "ROAD_NAME\r\n" + "IS_BASE\r\n" + "BUILD_NUM\r\n"
+            + "SUB_BUILD_NUM\r\n" + "BUILDING_NAME\r\n" + "DETAIL_BUILDING_NAME\r\n" + "ADDR_CODE";
+                                       
+      String[] keys = key.split("\r\n");
 
-		File path = new File("c:\\\\studyfile\\\\address");
-		List<File> fList = new ArrayList<>();
-		if (path.isDirectory()) {
-			File[] files = path.listFiles();
-			for (File file : files) {
-				if (!file.isDirectory() && file.getName().indexOf("build_") == 0) {
-					fList.add(file);
-				}
-
-			}
->>>>>>> branch 'master' of https://github.com/buleehdwp/erp.git
-		}
-		System.out.println(fList);
-		for (File f : fList) {
-			List<Map<String, String>> list = new ArrayList<>();
-			String str = ReadText.readText(path + "\\" + f.getName());
-			String[] strs = str.split("<>");
-			String[] values = null;
-			for (int i = 0; i < strs.length; i++) {
-				Map<String, String> map = new LinkedHashMap<>();
-				values = strs[i].split("\\|");
-				for (int j = 0; j < keys.length; j++) {
-					map.put(keys[j], values[j]);
-				}
-				list.add(map);
-			}
-			System.out.println(list);
-			
-			System.out.println();
-			long sTime = System.currentTimeMillis();
-			String sql = " insert into address(";
-			String value = " values(";
-			for (String Key : keys) {
-				sql += Key + ",";
-				value += "?,";
-			}
-			sql = sql.substring(0, sql.length() - 1) + ")\r\n";
-			value = value.substring(0, value.length() - 1) + ")";
-			sql += value;
-			Connection conn = Connector.open();
-			PreparedStatement ps = conn.prepareStatement(sql);
-			for (Map<String, String> row : list) {
-				for (int i = 0; i < keys.length; i++) {
-					ps.setString((i + 1), row.get(keys[i]));
-				}
-				ps.executeUpdate();
-
-			}
-			conn.commit();
-			long eTime = System.currentTimeMillis();
-			System.out.println("실행시간 : " + (eTime - sTime));
-		}
-	}
+      File path = new File("C:\\java_study_file\\address");
+      List<File> fList = new ArrayList<>();
+      if (path.isDirectory()) {
+         File[] files = path.listFiles();
+         for (File file : files) {
+            if (!file.isDirectory() && file.getName().indexOf("build_") == 0) {
+               fList.add(file);
+            }
+         }
+      }
+      for (File f : fList) {
+         List<Map<String, String>> list = new ArrayList<>();
+         String str = ReadText.readText(path + "\\" + f.getName());
+         String[] strs = str.split("<>");
+         String[] values = null;
+         for (int i = 0; i < strs.length; i++) {
+            Map<String, String> map = new LinkedHashMap<>();
+            values = strs[i].split("\\|");
+            for (int j = 0; j < keys.length; j++) {
+               map.put(keys[j], values[j]);
+            }
+            list.add(map);
+         }
+         System.out.println();
+         System.out.println("프로그램 실행");
+         long sTime = System.currentTimeMillis();
+         String sql = " insert into address(";
+         String value = " values(";
+         for (String Key : keys) {
+            sql += Key + ",";
+            value += "?,";
+         }
+         sql = sql.substring(0, sql.length() - 1) + ")\r\n";
+         value = value.substring(0, value.length() - 1) + ")";
+         sql += value;
+         Connection conn = Connector.open();
+         PreparedStatement ps = conn.prepareStatement(sql);
+         int cnt = 1;
+         for (Map<String, String> row : list) {
+            for (int i = 0; i < keys.length; i++) {
+               ps.setString((i + 1), row.get(keys[i]));
+            }
+            ps.addBatch();
+            if (cnt % 1000 == 0) {
+               ps.executeBatch();
+               ps.clearBatch();
+            }
+            cnt++;
+         }
+         if(list.size()%1000!=0) {
+            ps.executeBatch();
+            ps.clearBatch();
+         }
+         conn.commit();
+         long eTime = System.currentTimeMillis();
+         System.out.println("실행시간 : " + (eTime - sTime));
+         System.out.println("프로그램 종료");
+         
+      }
+   }
 }
--- src/io/FolderTest.java
+++ src/io/FolderTest.java
@@ -6,25 +6,21 @@ import java.util.List;
 
 public class FolderTest {
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		File path = new File("c:\\address\\address");
-=======
-		File path = new File("c:\\\\studyfile\\\\address");
 		List<File> fList = new ArrayList<>();
->>>>>>> branch 'master' of https://github.com/buleehdwp/erp.git
-		if(path.isDirectory()) {
+		if (path.isDirectory()) {
 			File[] files = path.listFiles();
-			for(File file:files) {
-				if(!file.isDirectory() && file.getName().indexOf("build_")==0) {
+			for (File file : files) {
+				if (!file.isDirectory() && file.getName().indexOf("build_") == 0) {
 					fList.add(file);
 				}
-				
+
 			}
 		}
 		System.out.println(fList);
 //		System.out.println(path.isDirectory());
 //		System.out.println(path.isFile());
-		
+
 	}
 
 }
