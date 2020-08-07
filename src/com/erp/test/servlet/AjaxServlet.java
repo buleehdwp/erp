bd712ca876fb9a190e88c162bb6f44a59e43ccb4
Administrator <Administrator@Si7A-46>
2020-08-07 16:01:50 +0900

    a

--- src/com/erp/test/servlet/AjaxServlet.java
+++ src/com/erp/test/servlet/AjaxServlet.java
@@ -2,23 +2,33 @@ package com.erp.test.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.erp.test.service.SelectJibunAddressService;
+import com.erp.test.service.impl.SelectJibunAddressServiceImpl;
+import com.erp.test.util.JSON;
+
 public class AjaxServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private static SelectJibunAddressService saService = new SelectJibunAddressServiceImpl();
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		response.setContentType("application/json;charset=utf-8");
+		request.setCharacterEncoding("utf-8");
 		PrintWriter pw = response.getWriter();
-		pw.println("{\"key\":\"value\"}");
+		List<String> sidoList = saService.SidoSelectList(null);
+		pw.println(JSON.stringify(sidoList));
 	}
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		doGet(request, response);
 	}
 
